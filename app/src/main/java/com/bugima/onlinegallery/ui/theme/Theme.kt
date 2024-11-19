11bb7bfbb6762af7acbbe67cea424e4be0ebaf74
bugima <macbugajski@gmail.com>
2024-11-19 07:47:57 +0100

    theme, text, const, misc

--- app/src/main/java/com/bugima/onlinegallery/ui/theme/Theme.kt
+++ app/src/main/java/com/bugima/onlinegallery/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bugima.onlinegallery.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,44 +10,43 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Accent,
+    onPrimary = LightPrimary,
+    primaryContainer = AccentDark,
+    onPrimaryContainer = AccentLight,
+    background = DarkPrimary,
+    onBackground = LightPrimary,
+    surface = DarkSecondary,
+    onSurface = LightPrimary,
+    error = Error,
+    onError = LightPrimary,
+    errorContainer = ErrorDark,
+    onErrorContainer = ErrorLight,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Accent,
+    onPrimary = DarkPrimary,
+    primaryContainer = AccentLight,
+    onPrimaryContainer = AccentDark,
+    background = LightSecondary,
+    onBackground = DarkPrimary,
+    surface = LightPrimary,
+    onSurface = DarkSecondary,
+    error = Error,
+    onError = LightPrimary,
+    errorContainer = ErrorLight,
+    onErrorContainer = ErrorDark,
 )
 
 @Composable
 fun OnlineGalleryTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
--- app/src/main/java/com/bugima/onlinegallery/util/Const.kt
+++ app/src/main/java/com/bugima/onlinegallery/util/Const.kt
@@ -6,7 +6,16 @@ object Const {
     const val PAGE_SIZE = 20
 
     // Pagination Interceptor
-    const val LINK_HEADER_REGEX = "<([^>]+)>; rel=\"next\"" // Regex to get next page url from 'Link' header
+    const val LINK_HEADER_REGEX = "<([^>]+)>; rel=\"next\""
     const val LINK_HEADER = "Link"
     const val NEXT_PAGE_URL = "NextPageUrl"
+
+    // String
+    const val TEXT_MAX_LINE_ONE = 1
+    const val TEXT_MAX_LINE_TWO = 2
+
+
+    // Animation
+    const val SPIN_DEGREE = 1080f
+    const val SPIN_DURATION = 3000
 }
